21b2a1488383bb98c4ac9fa3a65bfefa467c9df4
Mauro <your.email@example.com>
2025-06-24 12:33:06 -0300

    add livro2 to biblioteca_cidade and update item management

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ biblioteca_cidade = Biblioteca("Biblioteca da cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do shopping")
 ""
 livro1 = Livro("O Senhor dos Anéis", "J. R. R. Tolkien", 40.0, "9788532511017")
+livro2 = Livro("1984", "George Orwell", 30.0, "9788532522921")
 revista1 = Revista("Revista de Tecnologia", "Tech Magazine", 15.0, "Quinta")
 
 biblioteca_cidade.adicionar_item(livro1)
+biblioteca_cidade.adicionar_item(livro2)
 biblioteca_cidade.adicionar_item(revista1)
+
 
 
 
