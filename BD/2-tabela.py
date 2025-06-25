e316dbe7a9e42773258bc552d035d0cefb1edd4f
Mauro <your.email@example.com>
2025-06-25 12:48:01 -0300

    create database and table structure for filmes

---
+++ BD/2-tabela.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# 1 - conectando no BD
+conexao = sqlite3.connect('titulo.db')
+
+# 2 - Criando o cursor
+
+cursor = conexao.cursor()
+
+# 3 - Criando a tabela
+
+cursor.execute(
+    '''
+        CREATE TABLE filmes(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            ano INTEGER NOT NULL,
+            nota REAL NOT NULL
+            );
+    '''
+)
+
+
+# 4 - Fecha conexão 
+conexao.close()
+print("Tabela criada com sucesso!")
